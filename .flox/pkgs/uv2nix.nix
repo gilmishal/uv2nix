a19f57a76613e3fa85c2328b333ed9e9f14ea7b3
Gil Mishal <gil@flox.dev>
2026-02-19 18:52:55 -0500

    chore: add flox environment and package definition for uv2nix Nix library

---
+++ .flox/pkgs/uv2nix.nix
@@ -0,0 +1,33 @@
+{
+  stdenv,
+  lib,
+}:
+stdenv.mkDerivation {
+  pname = "uv2nix";
+  version = "0-unstable-2026-02-20";
+
+  src = builtins.path {
+    path = ../..;
+    name = "uv2nix-source";
+    filter = path: type:
+      let
+        rel = lib.removePrefix (toString ../..) path;
+      in
+      rel == "/default.nix" || lib.hasPrefix "/lib" rel;
+  };
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  installPhase = ''
+    mkdir -p $out
+    cp default.nix $out/
+    cp -r lib $out/
+  '';
+
+  meta = {
+    description = "Ingest uv workspace using Nix";
+    homepage = "https://github.com/pyproject-nix/uv2nix";
+    license = lib.licenses.mit;
+  };
+}
